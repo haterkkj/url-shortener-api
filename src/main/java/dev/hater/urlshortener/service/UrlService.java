7c6321476018ea03d42e5a1175b24a3f4c42379c
haterkkj <thiagoferreira2004f@gmail.com>
2025-05-23 13:57:02 -0300

    /feat: implementing service for Url

---
+++ src/main/java/dev/hater/urlshortener/service/UrlService.java
@@ -0,0 +1,35 @@
+package dev.hater.urlshortener.service;
+
+import dev.hater.urlshortener.domain.Url;
+import dev.hater.urlshortener.dto.CreateShortUrlRequest;
+import dev.hater.urlshortener.repository.UrlRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UrlService {
+
+    private final UrlRepository urlRepository;
+
+    public Url create(CreateShortUrlRequest request){
+        Url url = new Url();
+        url.setUrl(request.getUrl());
+
+        return urlRepository.save(url);
+    }
+
+    public Url findById(String id){
+        return urlRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Url not found"));
+    }
+
+    public void deleteById(String id){
+        if (!urlRepository.existsById(id)) {
+            throw new EntityNotFoundException("Url not found");
+        }
+
+        urlRepository.deleteById(id);
+    }
+
+}
