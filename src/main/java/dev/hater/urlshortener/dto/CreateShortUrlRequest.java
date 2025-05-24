72bab548a6c3ab95330f72e0920515d22f0714f0
haterkkj <thiagoferreira2004f@gmail.com>
2025-05-24 11:04:02 -0300

    /feat: validate url on short url creation

--- src/main/java/dev/hater/urlshortener/dto/CreateShortUrlRequest.java
+++ src/main/java/dev/hater/urlshortener/dto/CreateShortUrlRequest.java
@@ -1,12 +1,21 @@
 package dev.hater.urlshortener.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 public class CreateShortUrlRequest {
 
-    @NonNull
+    @NotNull(message = "Url cannot be null")
+    @NotBlank(message = "Url cannot be blank")
+    @Pattern(
+            regexp = "^(https?://)(www\\.)?([a-zA-Z0-9\\-]+\\.)+[a-zA-Z]{2,}(:[0-9]{1,5})?" +
+                    "(/[a-zA-Z0-9\\-._~:/?#@!$&'()*+,;=%]*)?$",
+            message = "Url is not valid"
+    )
     private String url;
 
 }
+
