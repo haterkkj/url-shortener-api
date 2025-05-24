9e6321bf8bd888d5965d23f04169789aa86a1ccc
haterkkj <thiagoferreira2004f@gmail.com>
2025-05-24 15:14:07 -0300

    /feat: dtos documented

--- src/main/java/dev/hater/urlshortener/dto/CreateShortUrlRequest.java
+++ src/main/java/dev/hater/urlshortener/dto/CreateShortUrlRequest.java
@@ -1,11 +1,13 @@
 package dev.hater.urlshortener.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(description = "Request object to create a new shortened URL")
 public class CreateShortUrlRequest {
 
     @NotNull(message = "Url cannot be null")
@@ -15,6 +17,7 @@ public class CreateShortUrlRequest {
                     "(/[a-zA-Z0-9\\-._~:/?#@!$&'()*+,;=%]*)?$",
             message = "Url is not valid"
     )
+    @Schema(description = "Original URL", example = "https://www.example.com/")
     private String url;
 
 }
--- src/main/java/dev/hater/urlshortener/dto/ShortUrlCreatedResponse.java
+++ src/main/java/dev/hater/urlshortener/dto/ShortUrlCreatedResponse.java
@@ -1,12 +1,16 @@
 package dev.hater.urlshortener.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data @AllArgsConstructor
+@Schema(description = "Response object to correctly created short URL")
 public class ShortUrlCreatedResponse {
 
+    @Schema(description = "Short URL", example = "xyz000")
     private String id;
+    @Schema(description = "Original URL", example = "https://www.example.com/")
     private String url;
 
 }
