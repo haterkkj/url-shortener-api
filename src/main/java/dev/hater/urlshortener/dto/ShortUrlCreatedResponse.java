19c82633d3f2f94b22a60058a61e4e0ef21fa502
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-02 10:23:20 -0300

    /feat: add Url new variables in dto

--- src/main/java/dev/hater/urlshortener/dto/ShortUrlCreatedResponse.java
+++ src/main/java/dev/hater/urlshortener/dto/ShortUrlCreatedResponse.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data @AllArgsConstructor
 @Schema(description = "Response object to correctly created short URL")
 public class ShortUrlCreatedResponse {
@@ -12,5 +14,11 @@ public class ShortUrlCreatedResponse {
     private String id;
     @Schema(description = "Original URL", example = "https://www.example.com/")
     private String url;
+    @Schema(description = "Number of times the short URL was used", example = "100")
+    private Integer useCount;
+    @Schema(description = "Lifetime of the short URL in hours", example = "2")
+    private Integer lifetime;
+    @Schema(description = "Timestamp when the short URL was created (UTC, ISO-8601 format)")
+    private Instant createdAt;
 
 }
