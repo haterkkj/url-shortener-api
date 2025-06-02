35da1726e39b7b609ceb73c57a1c324ce04a9000
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-02 10:27:40 -0300

    /feat: implement function to find expired urls in database

--- src/main/java/dev/hater/urlshortener/repository/UrlRepository.java
+++ src/main/java/dev/hater/urlshortener/repository/UrlRepository.java
@@ -2,8 +2,18 @@ package dev.hater.urlshortener.repository;
 
 import dev.hater.urlshortener.domain.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, String> {
+
+    @Query(value = """
+            SELECT * FROM url
+            WHERE created_at + make_interval(hours => lifetime) < now()
+            """, nativeQuery = true)
+    List<Url> findExpiredUrls();
+
 }
